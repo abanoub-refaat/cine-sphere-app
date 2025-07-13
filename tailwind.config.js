b833d070b4c5e2ed0a16da165d3d56d7b494ef37
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-13 14:48:32 +0300

    add: tailwind.config.js file with the primary configurations for the project.

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#f4f0ff",
+          100: "#ece4ff",
+          200: "#dacdff",
+          300: "#c0a6ff",
+          400: "#a473ff",
+          500: "#8a3bff",
+          600: "#8014ff",
+          700: "#7300ff",
+          800: "#6101d6",
+          900: "#5103af",
+          950: "#300077",
+        },
+        bgDark: "#1A0B28",
+        white: "#FFFFFF",
+        starColor: "#FFC700",
+        black: "#000000",
+        glass: "#E6D7E433",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
