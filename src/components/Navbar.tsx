3b103c2e215c959d815f89e27817b5e0fc9ea733
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-13 15:47:14 +0300

    feat: add navigation logo and update Navbar component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex w-full justify-between flex-row">
+      <div className="">
+        <Image
+          src={"nav-logo.svg"}
+          alt="cine sphere logo"
+          width={187}
+          height={57}
+        />
+      </div>
+      <div className="flex">
+        <a href="">Upcomings</a>
+        <a href="">Shows</a>
+        <a href="">Fanarts</a>
+        <a href="">Plans</a>
+        <a href="">Community</a>
+        <a href="">Account</a>
+      </div>
+      <div className="flex">
+        <div className="search-bar">search</div>
+        <div className="button">button</div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
