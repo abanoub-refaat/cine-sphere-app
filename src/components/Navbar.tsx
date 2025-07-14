cb70cc1b8e12655b4c7017c988e57e960bd7f9b2
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-14 13:27:00 +0300

    fix: enhancing folder structure in public folder and updating the nessesarly implementations in the Navbar.tsx file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import React from "react";
-import Image from "next/image";
 import Button from "@/components/Button";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Navbar = () => {
   const router = useRouter();
   return (
-    <nav className="flex w-full justify-between items-center flex-row bg-primary-400 py-8 px-24 h-12">
+    <nav className="flex w-full justify-between items-center flex-row bg-primary-400 py-10 px-24 h-12">
       <div className="cursor-pointer">
-        <Image
-          src={"nav-logo.svg"}
+        <img
+          src={"icons/nav-logo.svg"}
           alt="cine sphere logo"
-          width={187}
-          height={1}
-          style={{ height: 30 }}
+          style={{ width: 187, height: 30 }}
           onClick={() => router.push("/")}
         />
       </div>
-      <div className="flex text-white gap-16 text-sm justify-center items-center">
-        <a className="hover:text-primary-700 hover:font-bold" href="">
-          Upcomings
-        </a>
-        <a href="">Shows</a>
-        <a href="">Fanarts</a>
-        <a href="">Plans</a>
-        <a href="">Community</a>
-        <a href="">Account</a>
+      <div className="hidden sm:flex text-white gap-16 text-sm justify-center items-center">
+        <Link href={""} className="hover:text-primary-700">
+          Upcomming
+        </Link>
+        <Link href={""} className="hover:text-primary-700">
+          Shows
+        </Link>
+        <Link href={""} className="hover:text-primary-700">
+          Fanarts
+        </Link>
+        <Link href={""} className="hover:text-primary-700">
+          Plans
+        </Link>
+        <Link href={""} title="Community" className="hover:text-primary-700">
+          Community
+        </Link>
+        <Link href={""} className="hover:text-primary-700">
+          Account
+        </Link>
       </div>
       <div className="flex gap-7 items-center">
-        <div className="hover:animate-pulse cursor-pointer">
-          <Image src={"search.svg"} width={20} height={20} alt="search" />
+        <div className="cursor-pointer">
+          <img
+            src={"icons/search.svg"}
+            style={{ width: 20, height: 20 }}
+            alt="search"
+          />
         </div>
         <Button title="Join Now" hasIcon={false} />
       </div>
