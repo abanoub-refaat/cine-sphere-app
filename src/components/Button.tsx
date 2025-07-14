394efa5f709a09fe0b75c1b50d5ae0b3034b3b5f
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-14 12:05:49 +0300

    feat: update Navbar component with new layout and add search button

---
+++ src/components/Button.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+
+interface Props {
+  title: string;
+  hasIcon: boolean;
+  iconName?: string;
+}
+
+function Button({ title, hasIcon, iconName }: Props) {
+  return (
+    <button
+      type="button"
+      className="btn bg-primary-600 text-white text-sm rounded-tr-2xl rounded-bl-2xl w-36
+          py-2 hover:bg-primary-700 active:bg-primary-700 flex justify-center gap-2"
+    >
+      {hasIcon ? (
+        <Image
+          src={iconName ?? ""}
+          alt={iconName ?? ""}
+          width={15}
+          height={15}
+          className="items-center"
+        />
+      ) : (
+        ""
+      )}
+      {title}
+    </button>
+  );
+}
+
+export default Button;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,38 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import Button from "@/components/Button";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
-    <nav className="flex w-full justify-between flex-row">
-      <div className="">
+    <nav className="flex w-full justify-between items-center flex-row bg-primary-400 py-8 px-24 h-12">
+      <div className="cursor-pointer">
         <Image
           src={"nav-logo.svg"}
           alt="cine sphere logo"
           width={187}
-          height={57}
+          height={1}
+          style={{ height: 30 }}
+          onClick={() => router.push("/")}
         />
       </div>
-      <div className="flex">
-        <a href="">Upcomings</a>
+      <div className="flex text-white gap-16 text-sm justify-center items-center">
+        <a className="hover:text-primary-700 hover:font-bold" href="">
+          Upcomings
+        </a>
         <a href="">Shows</a>
         <a href="">Fanarts</a>
         <a href="">Plans</a>
         <a href="">Community</a>
         <a href="">Account</a>
       </div>
-      <div className="flex">
-        <div className="search-bar">search</div>
-        <div className="button">button</div>
+      <div className="flex gap-7 items-center">
+        <div className="hover:animate-pulse cursor-pointer">
+          <Image src={"search.svg"} width={20} height={20} alt="search" />
+        </div>
+        <Button title="Join Now" hasIcon={false} />
       </div>
     </nav>
   );
