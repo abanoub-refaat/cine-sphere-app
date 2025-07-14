05370c20f4fb24b9bb499538b41bc6bd3c13e138
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-14 11:20:56 +0300

    fix: tailwind configuartion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return (
-    <div className="flex flex-col bg-black">
-      <Navbar />
-      <h1>Hello, world!</h1>
-    </div>
-  );
+  return <Navbar />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "/pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{ts,js,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
