e16a7600ca8daeb340ea662f6a7e61d4689ba07d
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-16 15:19:29 +0300

    refactor: responsive view for the accordion section on mobile

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -5,9 +5,10 @@ import React, { useState } from "react";
 interface Props {
   question: string;
   answer: string;
+  id: number;
 }
 
-function Accordion({ question, answer }: Props) {
+function Accordion({ question, answer, id }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="flex justify-between w-80 flex-col rounded-lg bg-opacity-55 bg-dark text-white p-5 m-2">
@@ -22,7 +23,11 @@ function Accordion({ question, answer }: Props) {
           style={{ width: 30, height: 30 }}
         />
       </button>
-      {isOpen ? <span className="text-lg font-medium mt-4">{answer}</span> : ""}
+      {isOpen && id == id ? (
+        <span className="text-lg font-medium mt-4">{answer}</span>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
