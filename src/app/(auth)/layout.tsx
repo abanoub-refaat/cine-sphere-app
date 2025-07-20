a3f6aa5b64d0847aad750d8e2d8694bbf1babb10
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-20 15:34:07 +0300

    refactor: update layout structure and improve styling for components

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,9 +3,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="bg-black">{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
